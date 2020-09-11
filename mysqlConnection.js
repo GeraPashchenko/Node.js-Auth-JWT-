af12fa0b5da29f970e7c17f48e5039b725a1a8dd
Heorhii Pashchenko <54940741+GeraPashchenko@users.noreply.github.com>
2020-09-11 10:17:58 +0300

    fix connection

--- mysqlConnection.js
+++ mysqlConnection.js
@@ -1,8 +1,8 @@
 const mysql = require('sync-mysql');
 
 module.exports.connection = new mysql({
-    'host': process.env.MYSQL_HOST,
-    'user': process.env.MYSQL_USER,
-    'password': process.env.MYSQL_PASSWORD,
-    'database': process.env.MYSQL_DB
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'password',
+    'database': 'test'
 });
